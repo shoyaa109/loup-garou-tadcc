ef406f24dc0e5a44549eb8ddd8b13c349eba2f80
shoyaa109 <seishi918@gmail.com>
2026-06-12 15:32:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Bot TADC démarré !");
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages
+    ]
+});
+
+client.once('ready', () => {
+    console.log(`Connecté en tant que ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
